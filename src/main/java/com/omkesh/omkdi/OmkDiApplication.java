3df96652f0b5df3016c1d42a6577652a7e2d9aea
OmkeshGatfane <gatfaneomkesh@gmail.com>
2021-08-22 15:28:51 +0530

    maven project created ...added project as maven project by using pom file. these was issue with m2_home path...it was resolved by selecting maven bundle in project configuration

---
+++ src/main/java/com/omkesh/omkdi/OmkDiApplication.java
@@ -0,0 +1,13 @@
+package com.omkesh.omkdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmkDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmkDiApplication.class, args);
+	}
+
+}
